f83283641ab381cd79c5b15d21725f97477ec6cb
Aditya Kumar <aditya26262626@gmail.com>
2026-02-20 16:55:20 +0530

    frontend and backend connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors"
 import orderRoutes from "./routes/Order.routes.js";
 import userRoutes from "./routes/User.routes.js";
 import driverRoutes from "./routes/Driver.routes.js";
@@ -10,7 +10,10 @@ import authRoutes from "./routes/Auth.routes.js";
 // import "./workers/Order.worker.js";
 
 const app = express();
-
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}));
 app.use(express.json());
 
 app.use("/api/orders", orderRoutes);
@@ -18,6 +21,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/drivers", driverRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/auth", authRoutes);
+
+
 
 app.get("/", (req, res) => {
   res.send("FleetOps Backend Running 🚀");
