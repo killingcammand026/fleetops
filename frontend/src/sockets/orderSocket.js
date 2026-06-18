d1a934b4c8f70d2c99ae3ed68046d0d1505d1bb3
Aditya Kumar <aditya26262626@gmail.com>
2026-06-18 09:55:46 +0530

    fix3

--- frontend/src/sockets/orderSocket.js
+++ frontend/src/sockets/orderSocket.js
@@ -3,6 +3,19 @@ import { io } from "socket.io-client";
 const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000", {
   transports: ["websocket", "polling"],
   withCredentials: true,
+  autoConnect: false,  // don't connect until we have a token
+  auth: {
+    token: localStorage.getItem("token")
+  }
 });
+
+export const connectSocket = () => {
+  socket.auth = { token: localStorage.getItem("token") };
+  socket.connect();
+};
+
+export const disconnectSocket = () => {
+  socket.disconnect();
+};
 
 export default socket;
